8b9ba48c23d4fd0a26b12843f89d51120be298df
Paulo Jorge Reis Pereira <paulo_pereira_6@hotmail.com>
2015-12-26 23:53:06 +0000

    added project

---
+++ CardsAgainstHumanity/app/src/main/java/com/example/asus/cardsagainsthumanity/GameActivity.java
@@ -0,0 +1,37 @@
+package com.example.asus.cardsagainsthumanity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        final ListView listView = (ListView) findViewById(R.id.answerList);
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        final AnswerArrayAdapter adapter = new AnswerArrayAdapter(this, values);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapter.itemClicked(position);
+            }
+        });
+    }
+}
