d630ab1bbf6a1bdfe127614dc5bc17c5a777720c
jbsimoes <jbsimoes@student.dei.uc.pt>
2015-12-28 22:08:47 +0000

    Initial implementation of wifi mechanism

--- CardsAgainstHumanity/app/src/main/java/com/example/asus/cardsagainsthumanity/MainActivity.java
+++ CardsAgainstHumanity/app/src/main/java/com/example/asus/cardsagainsthumanity/MainActivity.java
@@ -1,20 +1,57 @@
 package com.example.asus.cardsagainsthumanity;
 
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.example.asus.cardsagainsthumanity.config.Configuration;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private WifiP2pManager manager;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel channel;
+    private BroadcastReceiver receiver = null;
+
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set peer2peer actions
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
+        if (Configuration.isDeviceBridgingEnabled)
+        {
+            wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+            if (!wifiManager.isWifiEnabled())
+            {
+                wifiManager.setWifiEnabled(true);
+            }
+
+
+        }
     }
 
     @Override
---
+++ CardsAgainstHumanity/app/src/main/java/com/example/asus/cardsagainsthumanity/wifi/WifiBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.example.asus.cardsagainsthumanity.wifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+
+/**
+ * Created by jbsimoes on 28/12/15.
+ */
+public class WifiBroadcastReceiver extends BroadcastReceiver
+{
+    private WifiManager manager;
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+    }
+}
