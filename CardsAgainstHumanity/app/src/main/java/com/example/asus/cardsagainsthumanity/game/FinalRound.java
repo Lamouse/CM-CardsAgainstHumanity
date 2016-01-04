3a726c0626b6a7f01c2c1ab9f2cfebf56c3d3696
jbsimoes <jbsimoes@student.dei.uc.pt>
2016-01-04 01:13:11 +0000

    Changed final round screen

--- CardsAgainstHumanity/app/src/main/java/com/example/asus/cardsagainsthumanity/game/FinalRound.java
+++ CardsAgainstHumanity/app/src/main/java/com/example/asus/cardsagainsthumanity/game/FinalRound.java
@@ -1,5 +1,6 @@
 package com.example.asus.cardsagainsthumanity.game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ import com.example.asus.cardsagainsthumanity.R;
 import java.util.ArrayList;
 
 import com.example.asus.cardsagainsthumanity.game.utils.Game;
+import com.example.asus.cardsagainsthumanity.router.MeshNetworkManager;
 import com.example.asus.cardsagainsthumanity.router.Receiver;
 
 public class FinalRound extends AppCompatActivity implements ManagerInterface
@@ -26,6 +28,11 @@ public class FinalRound extends AppCompatActivity implements ManagerInterface
 
         Receiver.setActivity(this);
 
+        Intent intent = getIntent();
+        String winnerCardsID = intent.getStringExtra("winnerCardsID");
+        String winnerMac = intent.getStringExtra("winnerMac");
+        String[] separated = winnerCardsID.split(",");
+
         Button button = (Button) findViewById(R.id.show_dialog_box);
         button.setText(Game.deviceName + " - " + Game.scoreTable.get(Game.deviceName) + " pts");
 
@@ -37,6 +44,21 @@ public class FinalRound extends AppCompatActivity implements ManagerInterface
 
         playerNames = new ArrayList<String>(Game.scoreTable.keySet());
         playerPoints = new ArrayList<Integer>(Game.scoreTable.values());
+
+        TextView a1 = (TextView) findViewById(R.id.white1);
+        TextView a2 = (TextView)findViewById(R.id.white2);
+        TextView winner = (TextView) findViewById(R.id.winner_player);
+
+        a1.setText(separated[0]); //FIXME: Ir à BD
+        if (separated.length == 2)
+        {
+            a2.setText(separated[1]); //FIXME: Ir à BD
+        }
+        else
+        {
+            a2.setVisibility(View.GONE);
+        }
+        winner.setText(winnerMac);
     }
 
     @Override
